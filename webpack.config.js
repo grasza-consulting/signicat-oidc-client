3a8daeefcfc78b7878779d32d6158097c4488331
Kamil Bednarek <kamil@nexilo.uk>
2019-04-24 09:35:40 +0200

    Add webpack configuration and build dist for client

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+/* global __dirname, require, module*/
+
+const webpack = require('webpack');
+const path = require('path');
+const env = require('yargs').argv.env; // use --env with webpack 2
+const pkg = require('./package.json');
+
+let libraryName = pkg.name;
+
+let outputFile, mode;
+
+// if (env === 'build') {
+//   mode = 'production';
+//   outputFile = libraryName + '.min.js';
+// } else {
+//   mode = 'development';
+//   outputFile = libraryName + '.js';
+// }
+
+mode = 'development';
+outputFile = libraryName + `-${env}.js`;
+
+const config = {
+  mode: mode,
+  entry: __dirname + '/src/signicat.js',
+  devtool: 'source-map',
+  output: {
+    path: __dirname + '/dist',
+    filename: outputFile,
+    library: libraryName,
+    libraryTarget: 'umd',
+    umdNamedDefine: true
+  },
+  module: {
+    rules: [{
+      test: /(\.jsx|\.js)$/,
+      loader: 'babel-loader',
+      exclude: /(node_modules|bower_components)/
+    },
+    {
+      test: /(\.jsx|\.js)$/,
+      loader: 'eslint-loader',
+      exclude: /node_modules/
+    }]
+  },
+  resolve: {
+    modules: [path.resolve('./node_modules'), path.resolve('./src')],
+    extensions: ['.json', '.js']
+  },
+  plugins: [
+    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/)
+  ]
+};
+
+module.exports = config;
